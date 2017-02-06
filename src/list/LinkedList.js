19b52d9c3dbce42a024b7aafa19dccedb796a070
Matt Phillips <mattphillips1016@gmail.com>
2017-02-06 22:00:36 +0000

    Add LinkedList get function

--- src/list/LinkedList.js
+++ src/list/LinkedList.js
@@ -22,4 +22,22 @@ export default class LinkedList<T> {
       current.setNext(next);
     }
   }
+
+  get(index: number): T {
+
+    if (this.nodes == null || index < 0)
+      throw new Error(`Index out of bounds: ${index}`);
+
+    let current: Node<T> = this.nodes;
+    for (let i = 0; i < index; i++) {
+
+      if (current.getNext() != null)
+        current = current.getNext();
+
+      else
+        throw new Error(`Index out of bounds: ${index}`);
+    }
+
+    return current.getData();
+  }
 }
--- src/list/LinkedList.test.js
+++ src/list/LinkedList.test.js
@@ -6,6 +6,7 @@ import LinkedList from './LinkedList';
 
 test('should add first node to the head of the list', t => {
   const list: List<string> = new LinkedList();
+
   list.add('Hello');
 
   t.is(list.nodes.data, 'Hello');
@@ -14,6 +15,7 @@ test('should add first node to the head of the list', t => {
 
 test('should add new nodes to the last node', t => {
   const list: List<string> = new LinkedList();
+
   list.add('Hello');
   list.add('World');
   list.add('!');
@@ -23,3 +25,46 @@ test('should add new nodes to the last node', t => {
   t.is(list.nodes.next.next.data, '!');
 });
 
+test('Should throw index out of bounds error when trying to access an index on an empty list', t => {
+  const list: List<string> = new LinkedList();
+
+  const error = t.throws(() => {
+    list.get(0);
+  }, Error);
+
+  t.is(error.message, 'Index out of bounds: 0');
+});
+
+test('Should throw index out of bounds exception when trying to access an index less than zero', t => {
+  const list: List<string> = new LinkedList();
+
+  list.add('Hello');
+
+  const error = t.throws(() => {
+    list.get(-1);
+  }, Error);
+
+  t.is(error.message, 'Index out of bounds: -1');
+});
+
+test('Should throw index out of bounds exception when index is larger than list size', t => {
+  const list: List<string> = new LinkedList();
+
+  list.add('Zero');
+
+  const error = t.throws(() => {
+    list.get(1);
+  }, Error);
+
+  t.is(error.message, 'Index out of bounds: 1');
+});
+
+test('should return value at given index', t => {
+  const list: List<string> = new LinkedList();
+
+  list.add('A');
+  list.add('B');
+  list.add('C');
+
+  t.is(list.get(0), 'A');
+});
